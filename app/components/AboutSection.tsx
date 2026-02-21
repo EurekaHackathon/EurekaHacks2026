8c12d51adb70477afbaaa7eae3f5c4f1d1024e92
ultrablob <neelgillshah@gmail.com>
2026-02-21 15:28:11 -0500

    make border widths consistent

--- app/components/AboutSection.tsx
+++ app/components/AboutSection.tsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 
 export default function AboutSection() {
   return (
-    <div className="w-screen h-screen !p-12 " id="about">
-      <div className="flex flex-col sm:flex-row border-8 border-white w-full h-full relative bg-[#0a1c29] md:!pr-8">
+    <div className="w-screen h-screen !p-12" id="about">
+      <div className="flex flex-col sm:flex-row border-12 border-white w-full h-full relative bg-[#0a1c29] md:!pr-8">
         <div className="flex flex-col gap-4 !p-20 z-2 text-xl sm:w-[50%]">
           <h1 className="font-righteous text-5xl">Who are we?</h1>
           <p className="font-inter text-md">
