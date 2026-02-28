55ca89ee29a0349919123523dd70e7650e5f3c1d
ozzy <businessozzy27@gmail.com>
2026-02-28 15:16:11 -0500

    dashboard from 2025 unchanged

---
+++ app/(dashboard)/dashboard/settings/page.tsx
@@ -0,0 +1,9 @@
+import ComingSoon from "@/components/ComingSoon";
+
+export default async function ApplicationPage() {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <ComingSoon text="In development"/>
+        </div>
+    );
+}
---
+++ components/SignOutForm.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useActionState } from "react";
+import { logout } from "@/lib/actions/auth";
+import { Icon } from "@iconify/react";
+
+export function SignOutForm() {
+    const [state, formAction, pending] = useActionState(logout, {});
+
+    return (
+        <form action={formAction}>
+            <button type="submit" disabled={pending}
+                    className="text-gray-600 w-40 flex items-center justify-center gap-2 border font-semibold border-gray-300
+                     py-1 px-12 rounded-xl hover:border-secondary-500 duration-200">
+                {!pending && "Log out"}
+                {pending && "​"}
+                {pending && <Icon className="text-2xl md:text-3xl lg:text-4xl absolute" icon="codex:loader"/>}
+            </button>
+        </form>
+    );
+}
